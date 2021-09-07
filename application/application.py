c7e577ce5c4b2bbeb126f09391f24d5803ba2cd0
Kozlov Nikita <nukfeed@yandex.ru>
2021-09-07 15:50:23 +0300

    initial commit

---
+++ application/application.py
@@ -0,0 +1,10 @@
+class Application:
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def __del__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,6 @@
+import application.application as app
+
+
+if __name__ == "__main__":
+    app = app.Application()
+    app.start()
